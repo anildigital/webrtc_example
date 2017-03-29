91429e87555e39f57a66710d3a1694c0f56bafda
Anil Wadghule <anildigital@gmail.com>
2017-03-29 14:23:41 +0530

    Change host url

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :webrtc_example, WebrtcExample.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "mighty-tor-32675.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "phoenix-webrtc-example.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
